d96bc4a3a9eea82362dd0f6b77421b5393a0637b
Vanja D <vanja.d@etheric.agency>
2020-08-04 22:21:43 +0200

    Fixed lambda to support pointer return types

--- fn/lambda.c
+++ fn/lambda.c
@@ -1,6 +1,17 @@
 macro lambda {
   case {
     match {
+      $(args) -> $(ret list) $(body)
+    }
+    template {
+      $(@getsym lambda 0)
+    }
+    toplevel {
+      $(@splice ret) $(@gensym lambda) $(args) $(body)
+    }
+  }
+  case {
+    match {
       $(args) -> $(ret) $(body)
     }
     template {
